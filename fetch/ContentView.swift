abc86bd56ab3fe68b091e34e5fb572589899b513
Travis <tkess17@gmail.com>
2025-01-30 16:53:26 -0600

    initial commit with project created files

---
+++ fetch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fetch
+//
+//  Created by Travis Kessinger on 1/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fetch/fetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchApp.swift
+//  fetch
+//
+//  Created by Travis Kessinger on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct fetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
